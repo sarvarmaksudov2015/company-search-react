69e513e4162dc8887f9442811d3121d50196590a
sarvarmaksudov2015 <sarvarmaksudov2015@gmail.com>
2020-08-12 06:50:20 -0400

    Added more info and style  in Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,14 +5,29 @@ const Modal = (props) => {
         return <div > < /div>
     }
     return <div style = {
-            { position: "fixed", top: 0, left: 0 } } >
+            { position: "fixed", top: 0, left: 0, margin: "20px", padding: "30px", width: "90%", height: "80%", backgroundColor: "lightgrey" } } >
         <
-        div onClick = {
+        p onClick = {
             () => {
                 props.setObject(null)
             }
-        } > Close < /div> <
-        div > { props.object.name } < /div> <
+        }
+    style = {
+            { color: "red" } } > Close < /p> <
+        h4 > Company Information < /h4><hr / >
+        <
+        h4 > { props.object.name } < /h4> <
+        p > Company status: < span style = {
+            { color: "green" } } > ACTIVE < /span></p >
+        <
+        h4 > COMPANY REGISTRATION NUMBER: { props.object.registrationNumber } < /h4>  <
+        h4 > VAT NUMBER: { props.object.vatNumber } < /h4> <
+        h4 > REGISTERED ADDRESS: { props.object.address } < /h4> <
+        h4 > COUNTRY: { props.object.country } < /h4> <
+        h4 > ADDITIONAL STATUS DETAILS: { props.object.additionalStatusDetails } < /h4> <
+        h5 > Headquaters Expired < /h5> <
+        h4 > COMPANY DESCRIPTION < /h4> <
+        div > { props.object.description } < /div> <
         /div>
 }
 export default Modal;
